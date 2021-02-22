237ce9e927d4105c3f9e87654acd8a50c2476017
sukaynaghnemat <sukaynaghnemat@yahoo.com>
2021-02-22 16:25:46 +0200

    modify js file

--- js/app.js
+++ js/app.js
@@ -1,219 +1,152 @@
-'use strict';
+"use strict";
 
-let maxClick=25;
-let attempt=0;
-let arrayObject=[];
+let maxClick = 25;
+let attempt = 1;
+let arrayObject = [];
+let arrayOfvote = [];
+let arrayOfShown = [];
 
-let leftImg= document.getElementById('leftImg');
-let middleImg= document.getElementById('middleImg');
-let rightImg= document.getElementById('rightImg');
+let leftImg = document.getElementById("leftImg");
+let middleImg = document.getElementById("middleImg");
+let rightImg = document.getElementById("rightImg");
 
-function busMall (name, source){
-    this.name= name;
-    this.source= source;
-    this.vote=0;
-    this.numOfShown =0;
-    
+function busMall(name, source) {
+  this.name = name;
+  this.source = source;
+  this.vote = 0;
+  this.numOfShown = 0;
 
-    arrayObject.push(this);
-
-
+  arrayObject.push(this);
 }
 
-new busMall ('bag', '/Imgs/bag.jpg');
-new busMall ('banana', '/Imgs/banana.jpg');
-new busMall ('bathroom', '/Imgs/bathroom.jpg');
-new busMall ('boots', '/Imgs/boots.jpg');
-new busMall ('breakfast', '/Imgs/breakfast.jpg');
-new busMall ('bubblegum', '/Imgs/bubblegum.jpg');
-new busMall ('chair', '/Imgs/chair.jpg');
-new busMall ('cthulhu', '/Imgs/cthulhu.jpg');
-new busMall ('dog-duck', '/Imgs/dog-duck.jpg');
-new busMall ('dragon', '/Imgs/dragon.jpg');
-new busMall ('pen', '/Imgs/pen.jpg');
-new busMall ('pet-sweep', '/Imgs/pet-sweep.jpg');
-new busMall ('scissors', '/Imgs/scissors.jpg');
-new busMall ('shark', '/Imgs/shark.jpg');
-new busMall ('sweep', '/Imgs/sweep.jpg');
-new busMall ('tauntaun', '/Imgs/tauntaun.jpg');
-new busMall ('unicorn', '/Imgs/unicorn.jpg');
-new busMall ('usb', '/Imgs/usb.gif');
-new busMall ('water-can', '/Imgs/water-can.jpg');
-new busMall ('wine-glass', '/Imgs/wine-glass.jpg');
-
+new busMall("bag", "/Imgs/bag.jpg");
+new busMall("banana", "/Imgs/banana.jpg");
+new busMall("bathroom", "/Imgs/bathroom.jpg");
+new busMall("boots", "/Imgs/boots.jpg");
+new busMall("breakfast", "/Imgs/breakfast.jpg");
+new busMall("bubblegum", "/Imgs/bubblegum.jpg");
+new busMall("chair", "/Imgs/chair.jpg");
+new busMall("cthulhu", "/Imgs/cthulhu.jpg");
+new busMall("dog-duck", "/Imgs/dog-duck.jpg");
+new busMall("dragon", "/Imgs/dragon.jpg");
+new busMall("pen", "/Imgs/pen.jpg");
+new busMall("pet-sweep", "/Imgs/pet-sweep.jpg");
+new busMall("scissors", "/Imgs/scissors.jpg");
+new busMall("shark", "/Imgs/shark.jpg");
+new busMall("sweep", "/Imgs/sweep.png");
+new busMall("tauntaun", "/Imgs/tauntaun.jpg");
+new busMall("unicorn", "/Imgs/unicorn.jpg");
+new busMall("usb", "/Imgs/usb.gif");
+new busMall("water-can", "/Imgs/water-can.jpg");
+new busMall("wine-glass", "/Imgs/wine-glass.jpg");
 
 ////////////three randome img
+let leftImgIndex;
+let middleImgIndex;
+let rightImgIndex;
 
+function renderThreeRandomImgs() {
+  
+    leftImgIndex=generatRandomIndex();
+    middleImgIndex=generatRandomIndex();
+    rightImgIndex=generatRandomIndex();
 
-function renderThreeRandomImgs(){
-    
-    let leftImg = generatRandomIndex();
-    let middleImg = generatRandomIndex();
-    let rightImg = generatRandomIndex();
+  if (leftImgIndex === middleImgIndex || leftImgIndex === rightImgIndex) {
+    leftImgIndex = generatRandomIndex();
+    //arrayObject[leftImgI].vote++;
+  } else if (
+    middleImgIndex === leftImgIndex ||
+    middleImgIndex === rightImgIndex
+  ) {
+    middleImgIndex = generatRandomIndex();
+    //arrayObject[middleImgI].vote++;
+  } else if (
+    rightImgIndex === leftImgIndex ||
+    rightImgIndex == middleImgIndex
+  ) {
+    rightImgIndex = generatRandomIndex();
+    //arrayObject[rightImgtIndex].vote++;
+  }
 
-    if ( leftImg === middleImg || leftImg === rightImg){
-        leftImg = generatRandomIndex();
-        arrayObject[leftImgI].vote++;
+  console.log(leftImgIndex);
+  console.log(middleImgIndex);
+  console.log(rightImgIndex);
+  let pictureLeft = document.getElementById("leftImg");
 
-    }
-    else if ( middleImg === leftImg || middleImg === rightImg){
+  let pictureMiddle = document.getElementById("middleImg");
 
-        middleImg = generatRandomIndex();
-        arrayObject[leftImgI].vote++;
+  let pictureRight = document.getElementById("rightImg");
 
-    }
+  pictureLeft.setAttribute("src", arrayObject[leftImgIndex].source);
+  arrayObject[leftImgIndex].numOfShown++;
 
+  pictureMiddle.setAttribute("src", arrayObject[middleImgIndex].source);
+  arrayObject[middleImgIndex].numOfShown++;
 
-    let pictureLeft = document.getElementById('leftImg');
-
-    let pictureMiddle = document. getElementById ('middleImg');
-
-    let pictureRight = document . getElementById ('rightImg');
-
-    pictureLeft .setAttribute('src' , arrayObject[leftImg].source);
-    
-    pictureMiddle .setAttribute('src' , arrayObject[middleImg].source);
-    
-    pictureRight .setAttribute('src' , arrayObject[rightImg].source);
-    
-
-
-
-
-
-
+  pictureRight.setAttribute("src", arrayObject[rightImgIndex].source);
+  arrayObject[rightImgIndex].numOfShown++;
 }
-
-
 
 renderThreeRandomImgs();
 
 // generat random value
 
-function generatRandomIndex (){
+function generatRandomIndex() {
+  let randomIndex = Math.floor(Math.random() * arrayObject.length);
 
-     let randomIndex = Math.floor(Math.random() * arrayObject.length);
-
-     return randomIndex;
+  return randomIndex;
 }
 
+leftImg.addEventListener("click", clickingOn);
 
+middleImg.addEventListener("click", clickingOn);
 
-
- leftImg = addEventListener('click' , clickingOn);
- middleImg = addEventListener('click' , clickingOn);
- rightImg = addEventListener('click' , clickingOn);
-
+rightImg.addEventListener("click", clickingOn);
 
 /////// event on click
 
-function clickingOn(event){
+function clickingOn(event) {
+   attempt++;
 
-    attempt++;
-
-    if(event.target.id === leftImg){
-
-        arrayObject[leftImgIndex].vote++;
-
-
+  console.log("event");
+  if (attempt < maxClick) {
+    // console.log(attempt)
+    //attempt++;
+   // console.lof('inside');
+    if (event.target.id === "leftImg") {
+      arrayObject[leftImgIndex].vote++;
+    } else if (event.target.id === "middleImg") {
+      arrayObject[middleImgIndex].vote++;
+    } else if (event.target.id === "rightImg") {
+      arrayObject[rightImgIndex].vote++;
     }
 
-    else if(event.target.id === middleImg){
+    renderThreeRandomImgs();
+  } else {
+    leftImg.removeEventListener("click", clickingOn);
 
-        arrayObject[middleImgIndex].vote++;
-    }
+    middleImg.removeEventListener("click", clickingOn);
 
-    else if (event.target.id === rightImg){
+    rightImg.removeEventListener("click", clickingOn);
 
+    //////creat un order list
 
-        arrayObject[rightImgtIndex].vote++;
-    
-    }
-    else {
-          //////creat un order list
+    let unOrderList = document.getElementById("unOrderList");
 
-        let unOrderList = document.getElementById('unOrderList');
-
-        let li;
-        for (let i=0 ; i< arrayObject.length ; i ++){
-
-            li = document .createElement ('li');
-            unOrderList.append(li);
-            li . textContent= `${arrayObject[i].name} had ${arrayObject[i].vote} votes, and was seen
+    let li;
+    for (let i = 0; i < arrayObject.length; i++) {
+      li = document.createElement("li");
+      unOrderList.append(li);
+      li.textContent = `${arrayObject[i].name} had ${arrayObject[i].vote} votes, and was seen
              ${arrayObject[i].numOfShown} times.`;
-
-        }
-
-
     }
-   
 
-    leftImg .removeEventListener('click', clickingOn);
-    middleImg .removeEventListener('click', clickingOn);
-    rightImg .removeEventListener('click', clickingOn);
+    for (let v = 0; v < arrayObject.length; v++) {
+      arrayOfvote.push(arrayObject[v].vote);
+      arrayOfShown.push[arrayObject[v].numOfShown];
+    }
+  }
 
-    let resultButton= document.getElementById('btn');
-    resultButton.addEventListener('click' , clickingOn);
-    
-
-
+ let resultButton = document.getElementById("bnt");
+resultButton.addEventListener("click", clickingOn);
 }
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
